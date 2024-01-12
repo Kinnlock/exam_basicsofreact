e7f2c4bb8e46925622507750a3fa0a48afb72c69
Nacer Eddine Attar <attar.nacereddine@gmail.com>
2024-01-12 11:22:25 +0100

    20%

--- src/components/AddCar.js
+++ src/components/AddCar.js
@@ -1,34 +1,131 @@
 import { useState, useRef } from 'react';
-const AddCar = () => {}
-// MARQUE ET MODELE EN LISTE DEROULANTE OK
+const AddCar = () => {
 
-// KILOMETRAGE ET PUISSANCE FISCALE =/= SAISIE DE VALEUR
+    const [formData, setFormData] = useState({
+        Brand: '',
+        CarModel: '',
+        Year: 2000,
+        Mileage: 0,
+        CV: 0,
+        Price: 0,
+        Title: '',
+        Description : ''
+      });
 
-//TYPE DE CARBURANT = PLUSIEUR BOUTON DISPONIBLE ON PEUT EN CHOISIR 1 (FAIRE DISPARAITRE LES AUTRES ? OU JUSTE STYLISER ?)
+    const handleFormData = e =>{ 
+        console.log(e.target.value);
+    setFormData(prevState => ({
+    ...prevState,
+    [e.target.name]: e.target.value,
+    }));}
+       
 
-//BOITE DE VITESSE ON PEUT EN CHOISIR 1 (FAIRE DISPARAITRE LES AUTRES ? OU JUSTE STYLISER ?)
+    const AddCar = e => {
+        e.preventDefault();
+        
+        // reinit values of formula
+        setFormData({
+            Brand: '',
+            CarModel: '',
+            Year: 2000,
+            Mileage: 0,
+            CV: 0,
+            Price: 0,
+            Title: '',
+            Description : ''
+        });
+        
+        // preparing data to server
+    };
+    const validateForm = () => {
+    // Vérifier si les champs sont vides
+        if (formData.champ1 === '' || formData.champ2 === '') {
+        alert('Veuillez remplir tous les champs.');
+        } else {
+        // Traitement du formulaire ici (par exemple, soumission du formulaire)
+        console.log('Formulaire soumis avec succès!', formData);
+        };
+    };  
+        
 
-//ETAT IDEM
+    return (
+        <form onSubmit={AddCar}>
+            <h1>Détails de l'annonce</h1>
+            <h6>Ajouter plus de détails sur votre annonce pour un maximum de visibilité</h6>
 
-//ORIGINE IDEM
+        {/* MARQUE, MODELE, ANNEE, KILOMETRAGE ET PUISSANCE FISCALE */}
+        <div>
+            <label>Marque </label>
+                <br></br>
+                <input
+                    value={formData.Brand}
+                    onChange={handleFormData}
+                    name='Brand'
+                />
+            <br></br>
+                <label>Modèle </label>
+            <br></br>
+                <input
+                    value={formData.CarModel}
+                    onChange={handleFormData}
+                    name='CarModel'
+                />
+            <br></br>
+                <label>Année </label>
+            <br></br>
+                <input
+                    type='number'
+                    value={formData.Year}
+                    onChange={handleFormData}
+                    name='Year'
+                />
+            <br></br>
+                <label>Kilométrage </label>
+            <br></br>
+                <input
+                    type='number'
+                    value={formData.Mileage}
+                    onChange={handleFormData}
+                    name='Mileage'
+                />      
+            <br></br>
+                <label>Puissance Fiscale </label>
+            <br></br>
+                <input
+                    type='number'
+                    value={formData.CV}
+                    onChange={handleFormData}
+                    name='CV'
+                />       
+        </div>
 
-//NOMBRE DE PORTES IDEM
+        {/* //TYPE DE CARBURANT = PLUSIEUR BOUTON DISPONIBLE ON PEUT EN CHOISIR 1 (FAIRE DISPARAITRE LES AUTRES ? OU JUSTE STYLISER ?) */}
 
-//PREMIERE MAIN IDEM
+        {/* //BOITE DE VITESSE ON PEUT EN CHOISIR 1 (FAIRE DISPARAITRE LES AUTRES ? OU JUSTE STYLISER ?) */}
 
-//=================INFO ANNONCE====================//
+        {/* //ETAT IDEM */}
 
-//PRIX EN SAISIE DE VALEUR (CHOISIR DEVISE AVEC CONVERSION ?)
+        {/* //ORIGINE IDEM */}
 
-//TITRE DE L'ANNONCE
+        {/* //NOMBRE DE PORTES IDEM */}
 
-//DESCRIPTION
+        {/* //PREMIERE MAIN IDEM */}
 
-//PLUS DE DETAILS SELECTION MULTIPLE DE BOUTONS
+        {/* //=================INFO ANNONCE====================// */}
 
+        {/* //PRIX EN SAISIE DE VALEUR (CHOISIR DEVISE AVEC CONVERSION ?) */}
 
+        {/* //TITRE DE L'ANNONCE */}
 
+        {/* //DESCRIPTION */}
 
+        {/* //PLUS DE DETAILS SELECTION MULTIPLE DE BOUTONS */}
+            <button type="button" onClick={validateForm}>
+                Valider
+            </button>
+        </form>
+    );
+};
 
 
 export default AddCar;
